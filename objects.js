2446a4d8e83055f552a3f265f9fa6a6a9c5803d3
unknown <isaac.asher97@gmail.com>
2023-05-09 12:03:07 -0400

    me me me completed

--- objects.js
+++ objects.js
@@ -1,5 +1,24 @@
+// List and describe each individual piece of syntax that we use to construct an object. Don't leave anything out! The list is finite.
+
 // const obj:keywords used to create an object in your program
 // {} - curly brackets are used to define the object
 // name: Isaac - name is considered a key property of the object, whereas "Isaac" is considered a value of the key withing the object
 
 //================================================================================
+{
+    const me = { // 1. Create an empty object called me.
+        firstName: "Isaac",
+        lastName: "Asher", // 2. Assign it properties for firstName, lastName, age, and email with corresponding values.
+        age: 26,
+        email: "isaac.asher97@gmail.com",
+    }
+    let newMe = { // 4. Without writing directly into the object, update the value of age to be 1000 years old.
+        me,
+        age: 1000,
+        "place of residence": "Brooklyn, Ny", // 6. Add a key to this object called: "place of residence" and give it a value of your hometown. Note that the key has spaces, therefore you cannot use dot notation.
+    }
+    console.log(me.firstName); // 3. Using dot notation, access the firstName property in your object.
+    console.log(newMe.age); // 5. Using dot notation, verify that age has been updated.
+    console.log(newMe["place of residence"]); // Access the value of "place of residence"
+    console.log(me) // returns => {firstName: "Isaac", lastName: "Asher",  age: 26, email: "isaac.asher97@gmail.com"}
+}
