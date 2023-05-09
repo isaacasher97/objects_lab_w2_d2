ae649d45bab7430a6bda81b5f5ec280df96b8737
unknown <isaac.asher97@gmail.com>
2023-05-09 10:53:57 -0400

    Objects Lab Syntax Answered

---
+++ objects.js
@@ -0,0 +1,5 @@
+// const obj:keywords used to create an object in your program
+// {} - curly brackets are used to define the object
+// name: Isaac - name is considered a key property of the object, whereas "Isaac" is considered a value of the key withing the object
+
+//================================================================================
